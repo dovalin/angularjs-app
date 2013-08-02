6bd9e1594c6d8f598e33e92e86561b8f4776b5fa
dovalin <alberto.doval@gmail.com>
2013-08-03 00:09:45 +0200

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularjsAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'Alberto',
+      'Doval',
+      '@dovalin'
+    ];
+  });
